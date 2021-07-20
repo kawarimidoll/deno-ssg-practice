75f7d0213abfad1da193e9d4471f6da0f92e21d1
kawarimidoll <kawarimidoll+git@gmail.com>
2021-07-20 16:53:01 +0900

    add html helpers

--- main.ts
+++ main.ts
@@ -1,3 +1,4 @@
+// deno-lint-ignore-file no-unused-vars
 import { Marked } from "https://deno.land/x/markdown@v2.0.0/mod.ts";
 // import { ensureFileSync } from "https://deno.land/std@0.102.0/fs/mod.ts";
 
@@ -27,6 +28,16 @@ if (!filename || !filename.endsWith(".md")) {
 }
 
 const COMPONENT_DELIMITER = "+++";
+const LAYOUT_PREFIX = "layout";
+const HOME_PATH = "/home";
+const STYLESHEET_PATH = "styles.css";
+const isHomePath = (path: string) => path === HOME_PATH;
+const getStylesheetHref = (path: string) =>
+  (isHomePath(path) ? "" : "../") + STYLESHEET_PATH;
+const getFaviconSvg = (emoji?: string) =>
+  `<svg xmlns="http://www.w3.org/2000/svg"><text y="32" font-size="32">${emoji ||
+    "🦕"}</text></svg>`;
+
 const content = await Deno.readTextFile(filename);
 const components = content.split(COMPONENT_DELIMITER);
 const { meta: frontMatter } = Marked.parse(components[0]);
@@ -41,7 +52,8 @@ components.forEach((component) => {
   const [path, name] = match[1].replace(/<[^>]+>/g, "").split(":");
   const html = match[2].replace(/\n/g, "");
   // console.log({ path, name, html });
-  if (path === "layout") {
+
+  if (path === LAYOUT_PREFIX) {
     layout[name] = html;
   } else {
     pages.push({ path, name, html });
