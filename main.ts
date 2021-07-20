f7846e247d8eda2eefd33cd51cf3568453ce04df
kawarimidoll <kawarimidoll+git@gmail.com>
2021-07-20 10:16:11 +0900

    add dependencies, interfaces, globals

--- main.ts
+++ main.ts
@@ -1,3 +1,21 @@
+// deno-lint-ignore-file no-unused-vars
+import { Marked } from "https://deno.land/x/markdown@v2.0.0/mod.ts";
+import { ensureFileSync } from "https://deno.land/std@0.102.0/fs/mod.ts";
+
+interface Page {
+  path: string;
+  name: string;
+  html: string;
+}
+
+interface Layout {
+  // deno-lint-ignore no-explicit-any
+  [key: string]: any;
+}
+
+const pages: Page[] = [];
+const layout: Layout = {};
+
 const [filename, buildPath = "./build"] = Deno.args;
 
 console.log(filename, buildPath);
