21a31217cac8a02f486a242e3de8097381db5247
kawarimidoll <kawarimidoll+git@gmail.com>
2021-07-20 13:41:44 +0900

    parse source markdown file

--- main.ts
+++ main.ts
@@ -1,4 +1,3 @@
-// deno-lint-ignore-file no-unused-vars
 import { Marked } from "https://deno.land/x/markdown@v2.0.0/mod.ts";
 // import { ensureFileSync } from "https://deno.land/std@0.102.0/fs/mod.ts";
 
@@ -32,5 +31,21 @@ const content = await Deno.readTextFile(filename);
 const components = content.split(COMPONENT_DELIMITER);
 const { meta: frontMatter } = Marked.parse(components[0]);
 const { title, styles, favicon } = frontMatter;
-console.log(components);
+// console.log(components);
 console.log({ title, styles, favicon });
+
+components.forEach((component) => {
+  const { content } = Marked.parse(component);
+
+  const match = content.match(/([^\n]*)\n(.*)/s) ?? [];
+  const [path, name] = match[1].replace(/<[^>]+>/g, "").split(":");
+  const html = match[2].replace(/\n/g, "");
+  // console.log({ path, name, html });
+  if (path === "layout") {
+    layout[name] = html;
+  } else {
+    pages.push({ path, name, html });
+  }
+});
+
+console.log({ pages, layout });
