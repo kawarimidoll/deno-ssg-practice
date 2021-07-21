a592cf85a04de4bbce0b3d9235338857f73a1eaf
kawarimidoll <kawarimidoll+git@gmail.com>
2021-07-21 09:43:13 +0900

    add config.ts

---
+++ config.ts
@@ -0,0 +1,4 @@
+export const SOURCE_DIR = "docs";
+export const BUILD_DIR = "build";
+export const SITE_NAME = "Deno SSG site";
+export const FAVICON = "🦕";
--- server.ts
+++ server.ts
@@ -3,10 +3,12 @@
 import mime from "https://cdn.skypack.dev/mime@2.5.2/lite?dts";
 import ky from "https://cdn.skypack.dev/ky@0.28.5?dts";
 
+import { BUILD_DIR } from "./config.ts";
+
 async function handleRequest(request: Request) {
   const { pathname } = new URL(request.url);
 
-  const filename = "./build" + (pathname === "/" ? "" : pathname) +
+  const filename = BUILD_DIR + (pathname === "/" ? "" : pathname) +
     (pathname.includes(".") ? "" : "/index.html");
 
   const file = new URL(filename, import.meta.url);
